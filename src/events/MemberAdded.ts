8cb84385f63691dd7515203c1826944b30131a5a
buddy-codes <vodlsreal@gmail.com>
2023-03-04 14:56:36 -0500

    💄 Pretty Member Join Embed

--- src/events/MemberAdded.ts
+++ src/events/MemberAdded.ts
@@ -4,7 +4,10 @@ import {
   GuildMember,
   TextChannel,
   EmbedBuilder,
-  Colors
+  Colors,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle
 } from "discord.js";
 
 export default (client: Client): void => {
@@ -18,16 +21,59 @@ export default (client: Client): void => {
       WELCOME_CHANNEL_ID
     );
 
-    await channel.send({
-      embeds: [
-        new EmbedBuilder()
-          .setTitle("👋 Member Joined")
-          .setDescription(
-            `Welcome to **${member.guild.name}**, <@${member.user.id}>!\n\nMake sure to read the rules in <#${RULES_CHANNEL_ID}> and check out <#${ANNOUNCEMENTS_CHANNEL_ID}> for updates!`
-          )
-          .setColor(Colors.Blurple)
-      ]
-    });
+    await channel
+      .send({
+        content: `<@${member.user.id}>`,
+        embeds: [
+          new EmbedBuilder()
+            .setTitle("👋 Member Joined")
+            .setDescription(
+              `Welcome to **${member.guild.name}**, <@${member.user.id}>!\n\nMake sure to read the rules in <#${RULES_CHANNEL_ID}> and check out <#${ANNOUNCEMENTS_CHANNEL_ID}> for updates!`
+            )
+            .setColor(Colors.Blurple)
+        ]
+      })
+      .then((m) => m.react("👋"));
     await member.roles.add("1077345475088355338");
+
+    try {
+      await member.send({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle("👋 Welcome to the server!")
+            .setDescription(
+              `Welcome to **${member.guild.name}**, <@${member.user.id}>!\n\nMake sure to read the rules in <#${RULES_CHANNEL_ID}> and check out <#${ANNOUNCEMENTS_CHANNEL_ID}> for updates!`
+            )
+            .setColor(Colors.Blurple)
+        ],
+        components: [
+          new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+              .setStyle(ButtonStyle.Link)
+              .setLabel("Rules")
+              .setURL(
+                "https://discord.com/channels/1077342825957576735/1077343081994657833"
+              )
+              .setEmoji("📜"),
+            new ButtonBuilder()
+              .setStyle(ButtonStyle.Link)
+              .setLabel("Announcements")
+              .setURL(
+                "https://discord.com/channels/1077342825957576735/1077343359171039343"
+              )
+              .setEmoji("📢"),
+            new ButtonBuilder()
+              .setStyle(ButtonStyle.Link)
+              .setLabel("General")
+              .setURL(
+                "https://discord.com/channels/1077342825957576735/1077343503908081724"
+              )
+              .setEmoji("💬")
+          )
+        ]
+      });
+    } catch (e) {
+      return;
+    }
   });
 };
