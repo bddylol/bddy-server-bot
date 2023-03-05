7e20e1a0e76d35e2cd424f3b10f8b0f30c77e817
buddy-codes <vodlsreal@gmail.com>
2023-03-05 16:35:14 -0500

    🐛 Process Events
    Co-authored-by: Saahil <NeonGamerBot-QK@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 require("dotenv/config");
-import { Client } from "discord.js";
+import { Client, codeBlock, EmbedBuilder } from "discord.js";
 import ClientReady from "./events/ClientReady";
 import CreateUserOnAnyAction from "./events/CreateUserOnAnyAction";
 import InteractionCreate from "./events/InteractionCreate";
@@ -19,5 +19,66 @@ ClientReady(client);
 MemberAdded(client);
 InteractionCreate(client);
 CreateUserOnAnyAction(client);
+
+process.on("uncaughtException", (e) => {
+  //@ts-ignore
+  client.channels.cache.get("1081932588161187851").send({
+    embeds: [
+      new EmbedBuilder()
+        .setTitle("Uncaught Exception")
+        //@ts-ignore
+        .setDescription(`${codeBlock(e)}`)
+        .setColor("Red")
+    ]
+  });
+});
+process.on("unhandledRejection", (e) => {
+  //@ts-ignore
+  client.channels.cache.get("1081932588161187851").send({
+    embeds: [
+      new EmbedBuilder()
+        .setTitle("Unhandled Rejection")
+        //@ts-ignore
+        .setDescription(`${codeBlock(e)}`)
+        .setColor("Red")
+    ]
+  });
+});
+process.on("uncaughtExceptionMonitor", (e) => {
+  //@ts-ignore
+  client.channels.cache.get("1081932588161187851").send({
+    embeds: [
+      new EmbedBuilder()
+        .setTitle("Uncaught Expection Monitor")
+        //@ts-ignore
+        .setDescription(`${codeBlock(e)}`)
+        .setColor("Red")
+    ]
+  });
+});
+process.on("warning", (e) => {
+  //@ts-ignore
+  client.channels.cache.get("1081932588161187851").send({
+    embeds: [
+      new EmbedBuilder()
+        .setTitle("Warning")
+        //@ts-ignore
+        .setDescription(`${codeBlock(e)}`)
+        .setColor("Yellow")
+    ]
+  });
+});
+process.on("exit", (e) => {
+  //@ts-ignore
+  client.channels.cache.get("1081932588161187851").send({
+    embeds: [
+      new EmbedBuilder()
+        .setTitle("Exit")
+        //@ts-ignore
+        .setDescription(`${codeBlock(e)}`)
+        .setColor("Red")
+    ]
+  });
+});
 
 client.login(process.env.DISCORD_TOKEN);
