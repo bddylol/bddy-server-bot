09f05215a76de4fc0d6202f8a0aaf08a209d5be5
buddy-codes <vodlsreal@gmail.com>
2023-03-05 13:00:45 -0500

    🚑 why is the bot trying to add a webhook to db

--- src/util/sql.ts
+++ src/util/sql.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const CreateUserInDB = async (user: GuildMember) => {
+  if (!user) return;
   if (user.user.bot) return;
   if (await prisma?.user?.findUnique({ where: { discord_id: user.id } }))
     return console.log(`[DB] User ${user.id} already exists in the database.`);
