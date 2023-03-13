c92175f9c6d28e05cae4ffb876d42571f885ef32
buddy-codes <vodlsreal@gmail.com>
2023-03-13 13:25:36 -0400

    🚀 Context Menus

--- src/events/InteractionCreate.ts
+++ src/events/InteractionCreate.ts
@@ -11,6 +11,35 @@ import {
 
 export default (client: Client): void => {
   client.on(Events.InteractionCreate, async (interaction) => {
+    // Context Menus
+    if (interaction.isContextMenuCommand()) {
+      const command = interaction.client.commands.get(interaction.commandName);
+      if (!command) {
+        console.error(
+          `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+      }
+
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        await interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setTitle("Internal Error")
+              .setDescription(`${codeBlock("js", `${error}`)}`)
+              .setColor(Colors.Red)
+              .setFooter({
+                text: "Please report this to bddy#5683",
+                iconURL: "https://bddy.lol/favicon.png"
+              })
+          ]
+        });
+      }
+    }
+
     //AutoComplete
     if (interaction.isAutocomplete()) {
       //@ts-ignore idk error
@@ -30,35 +59,35 @@ export default (client: Client): void => {
         console.error(error);
       }
     }
-    if (!interaction.isChatInputCommand()) return;
 
-    //@ts-ignore idk error
+    // Slash Comamnds
+    if (interaction.isChatInputCommand()) {
+      const command = interaction.client.commands.get(interaction.commandName);
 
-    const command = interaction.client.commands.get(interaction.commandName);
+      if (!command) {
+        console.error(
+          `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+      }
 
-    if (!command) {
-      console.error(
-        `No command matching ${interaction.commandName} was found.`
-      );
-      return;
-    }
-
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({
-        embeds: [
-          new EmbedBuilder()
-            .setTitle("Internal Error")
-            .setDescription(`${codeBlock("js", `${error}`)}`)
-            .setColor(Colors.Red)
-            .setFooter({
-              text: "Please report this to bddy#5683",
-              iconURL: "https://bddy.lol/favicon.png"
-            })
-        ]
-      });
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        await interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setTitle("Internal Error")
+              .setDescription(`${codeBlock("js", `${error}`)}`)
+              .setColor(Colors.Red)
+              .setFooter({
+                text: "Please report this to bddy#5683",
+                iconURL: "https://bddy.lol/favicon.png"
+              })
+          ]
+        });
+      }
     }
   });
 };
